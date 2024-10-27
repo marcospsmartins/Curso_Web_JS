bdc2a37dae7c275bcd7c3648b9c86c7f2f76f987
Marcos Paulo Sunto Martins <marcospsmartins@gmail.com>
2024-10-26 23:26:22 -0300

    js26/10/24

---
+++ inicio/05_desafio_sentenca.js
@@ -0,0 +1,21 @@
+{
+    // BLOBO DE CÓDIGOS 01 - NÚMEROS IMPARES
+    {
+        console.log("NUMEROS IMPARES")
+        console.log('1');
+        console.log('3');
+        console.log('5');
+        console.log('7');
+        console.log('9');
+    };
+
+    // BLOBO DE CÓDIGOS 02 - NÚMEROS PARES
+    {
+        console.log("NÚMEROS PARES")
+        console.log('2');
+        console.log('4');
+        console.log('6');
+        console.log('8');
+        console.log('10');
+    };
+};
